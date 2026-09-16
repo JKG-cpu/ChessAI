95bff47d5f83f3a727267411491342bfdde5437b
JKG-cpu <jkg66512@gmail.com>
2026-09-16 16:56:02 -0700

    Added a ClearTerminal Function

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"runtime"
+
 	"github.com/JKG-cpu/ChessAI/display"
 )
 
+func ClearTerminal() {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func main() {
 	display.RunDisplay()
+	ClearTerminal()
 }
